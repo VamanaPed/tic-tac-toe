889a9f57f77bc300e71a6e66602fa2e02c989e6b
VamanaPed <168412411+VamanaPed@users.noreply.github.com>
2024-11-26 11:14:43 +1300

    Board win condition Added board win condition checks

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ function makeBoard(size) {
 
   function placeTile(x, y, value) {
     if (tiles[y * size + x] === "") tiles[y * size + x] = value;
+    return checkWin(x, y, value);
+  }
+
+  function checkWin(xPos, yPos, symbol) {
+    for (let x = 0; x < size; x++) {
+      if (tiles[yPos * size + x] !== symbol) break;
+      if (x == size - 1) return true;
+    }
+
+    for (let y = 0; y < size; y++) {
+      if (tiles[y * size + xPos] !== symbol) break;
+      if (y == size - 1) return true;
+    }
+    for (let i = 0; i < size; i++) {
+      if (tiles[i * size + i] !== symbol) break;
+      if (i == size - 1) return true;
+    }
+
+    for (let i = 0; i < size; i++) {
+      if (tiles[(size - 1 - i) * size + i] !== symbol) break;
+      if (i == size - 1) return true;
+    }
   }
 
   function printBoard() {
@@ -31,5 +53,7 @@ const board = makeBoard(3);
 board.placeTile(0, 1, "X");
 board.placeTile(1, 1, "X");
 board.placeTile(2, 1, "X");
+board.placeTile(0, 0, "X");
+board.placeTile(2, 2, "X");
 
 board.printBoard();
